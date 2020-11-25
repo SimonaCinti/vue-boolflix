db64556cab7d5973b16f5a31676b480d8fc80435
Simona Cinti <simona.cinti2@gmail.com>
2020-11-25 12:13:06 +0100

    setup vue with basic info

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-console.log('hello', Vue);
+/*           **
+**           **
+**  Boolflix **
+**           **
+**           */
+
+
+//TODO: Milestone 1: 
+//TODO: - searchbar - print a schermo dei film con i seguenti valori: 1) Titolo 2) Titolo Originale 3) Lingua 4) Voto 
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        searchInput: '',
+    }
+})
