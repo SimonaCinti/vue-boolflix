249b8c6f5468f3c4e21403da12f499ad205ade4c
Simona Cinti <simona.cinti2@gmail.com>
2020-11-25 12:28:58 +0100

    check api

--- js/main.js
+++ js/main.js
@@ -11,6 +11,28 @@
 var app = new Vue({
     el: '#app',
     data: {
+        // Data raccolti
         searchInput: '',
+        // Array of movies
+        movies: [],
+    }, // end Data
+    methods:{
+        getResult(){
+            axios.get('https://api.themoviedb.org/3/search/movie',{
+                params: {
+                    api_key: '8ee3ad03988875233a1bddc1e4e8ff76',
+                    query: 'ritorno al futuro'
+                }
+            })
+                .then(response => {
+                    // handle success
+                    console.log(response.data.results);
+                    return this.movies = response.data.results
+                })
+                .catch(function (error) {
+                    // handle error
+                    console.log(error);
+                })
+        }
     }
 })
