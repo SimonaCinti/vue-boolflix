66a74a26b4a0edba88628dd3ca6a81ee9416c5d5
Simona Cinti <simona.cinti2@gmail.com>
2020-11-26 12:28:18 +0100

    poster for movies and tv series

--- js/main.js
+++ js/main.js
@@ -91,11 +91,18 @@ var app = new Vue({
         getFlag(lang){
             return `./img/${lang}.png`
         },
+        /** 
+         *  Check Poster
+        */
+        checkPoster(poster){
+            if (poster !== '')
+            return poster
+        },
         /**
          * Get poster image
          */
         getPoster(poster){
-            return `https://image.tmdb.org/t/p/w342/${poster}`
+            return `https://image.tmdb.org/t/p/w154/${poster}`
         },
     }
 })
