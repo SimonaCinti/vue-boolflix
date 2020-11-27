6cf2dc0d4505cbdb42553c3cdf97b03d2d97b383
Simona Cinti <simona.cinti2@gmail.com>
2020-11-27 13:14:23 +0100

    all options

--- js/main.js
+++ js/main.js
@@ -22,6 +22,27 @@ var app = new Vue({
         selectedGenre: 'all',
         // available genre
         movieGenre: [],
+        // all genres
+        allGenres: [
+            { "id": 28, "name": "Action" },
+            { "id": 12, "name": "Adventure" }, 
+            { "id": 16, "name": "Animation" }, 
+            { "id": 35, "name": "Comedy" }, 
+            { "id": 80, "name": "Crime" }, 
+            { "id": 99, "name": "Documentary" }, 
+            { "id": 18, "name": "Drama" }, 
+            { "id": 10751, "name": "Family" }, 
+            { "id": 14, "name": "Fantasy" }, 
+            { "id": 36, "name": "History" }, 
+            { "id": 27, "name": "Horror" }, 
+            { "id": 10402, "name": "Music" }, 
+            { "id": 9648, "name": "Mystery" }, 
+            { "id": 10749, "name": "Romance" }, 
+            { "id": 878, "name": "Science Fiction" }, 
+            { "id": 10770, "name": "TV Movie" }, 
+            { "id": 53, "name": "Thriller" }, 
+            { "id": 10752, "name": "War" }, 
+            { "id": 37, "name": "Western" }]
     }, // end Data
     methods:{
         /**
@@ -52,7 +73,7 @@ var app = new Vue({
                     this.movies = response.data.results;
                     //Copy genre available
                     this.movies.forEach((movie)=>{
-                        if (!this.movieGenre.includes(movie.genre_ids)){
+                        if (!this.movieGenre.includes(movie.genre_ids) && (movie.genre_ids.length !== 0 )){
                             this.movieGenre.push(movie.genre_ids);
                         }
                     })
